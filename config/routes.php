ccbc6895b9282e7be83d14389a4e793b4b6a6224
Subhan Ahmed <Subhan Ahmed>
2016-07-26 23:22:56 +0500

    Composer.phar removed

--- config/routes.php
+++ config/routes.php
@@ -63,7 +63,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['controller' => 'Dashboard', 'action' => 'index']);
 
     $routes->connect('/register', ['controller' => 'Users', 'action' => 'add'], ['_name' => 'register']);
 
